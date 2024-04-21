d024cac269e0cbdd9edfcb304b7251d42ac686a9
Макеева Олеся <makeeva.o.a@yandex.ru>
2024-04-21 09:13:10 +0300

    'first'

---
+++ config.py
@@ -0,0 +1,14 @@
+api_id = 
+api_hash = ''
+bot_token = ''
+my_id = 
+
+technical_channel_id =   # ID технического канала
+
+new_link = "http://t.me/"
+new_username = "@"
+
+new_word = '' # на какое слово заменить
+
+proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
